8c232908ef0b61376685f223b35bde8aa0e1da6c
juwan-25 <s2109@e-mirim.hs.kr>
2023-03-30 23:36:25 +0900

    form에서 비밀번호(평문) 받아오기

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
+const querystring = require('querystring');
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const iv = crypto.randomBytes(16);
+let plaintext, key;
+
+const encryption = require('./public/js/encryption.js');
+const decryption = require('.//public/js/decryption.js');
 
 const server = http.createServer(async (req, res)=>{
     try{
         console.log("URL : ",req.url);
-        if(req.method==='GET'){
-          let fileSet = req.url;
+        let fileSet = req.url;
 
-          if(req.url == '/favicon.ico'){
-              //현재는 사용하지 않아서 favicon이 나온다면 오류 발생
-              return res.writeHead(404);
-          } else if(req.url == '/') {
-              //fileSet 아무 입력이 없다면? 기본으로 index.html 호출
-              fileSet = "index";
-          }
+        if(req.url == '/favicon.ico'){
+            //현재는 사용하지 않아서 favicon이 나온다면 오류 발생
+            return res.writeHead(404).end();
+        } else if(req.url == '/') {
+            //fileSet 아무 입력이 없다면? 기본으로 index.html 호출
+            fileSet = "index.html";
+        } 
+        if(req.method=='POST'){
+            let body = '';
+            req.on('data', function (data) {
+                body = body + data;
+            });
+            req.on('end', function () {
+                let post = querystring.parse(body);
+
+                plaintext = post.plaintext;
+                key = crypto.scryptSync(plaintext,'salt', 32);     
+                
+                let cipher = encryption.e(plaintext,crypto, algorithm, key, iv);
+                            
+            });
+        }
           res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
   
           console.log("fileSet : ",fileSet, "__dirname : ", __dirname);
@@ -23,8 +46,9 @@ const server = http.createServer(async (req, res)=>{
           //원하는 파일을 열 수 있도록 함
           const data = await fs.readFile(path.join(__dirname,"/public/",fileSet)); // 절대경로
           //const data = await fs.readFile(`./html/${fileSet}.html`); //상대경로
+          
+          
           res.end(data);
-        }
     }catch(err){
         console.error(err);
         res.writeHead(200, {"Content-Type":"text/html"});
